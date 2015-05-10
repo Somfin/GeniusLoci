de8d23e22278c81131fec8e06eea159a49329dfc
seanmckeever16 <seanmckeever@Seans-MacBook-Pro-2.local>
2015-05-10 21:30:32 +1200

    Survival Shooter With Alternate Spawn Options
    The finished Survivor shooter tutorial, but can spawn enemies from
    button press or collider enter. Enemies follow player.

---
+++ Assets/Sean/Survival Shooter With Enemy Spawn/Assets/EnemyTrigger.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyTrigger : MonoBehaviour {
+
+	public GameObject obj;
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "Player") {
+			this.gameObject.SendMessageUpwards ("Spawn");
+		}
+	}
+}
