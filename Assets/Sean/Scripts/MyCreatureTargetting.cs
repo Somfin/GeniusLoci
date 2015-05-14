4d56ecb082007be978fbe07abb242a5bf67a5848
seanmckeever16 <seanmckeever@Seans-MacBook-Pro-2.local>
2015-05-14 14:53:04 +1200

    SpiderCrab added and animated

---
+++ Assets/Sean/Scripts/MyCreatureTargetting.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MyCreatureTargetting : MonoBehaviour {
+
+	private NavMeshAgent agent;
+	private GameObject target;
+	private Animator anim;
+	
+	// Use this for initialization
+	void Start () {
+		agent = GetComponent<NavMeshAgent> ();
+		target = GameObject.FindGameObjectWithTag ("Player");
+		anim = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Vector3.Distance (transform.position, target.transform.position) < 40f) {
+			anim.SetBool ("isMoving", true);
+			agent.SetDestination (target.transform.position);
+		} else
+			anim.SetBool ("isMoving", false);
+	}
+}
