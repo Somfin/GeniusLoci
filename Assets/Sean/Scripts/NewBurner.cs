6589aedf2dc6c6aa9de8a79cdbbd678d573e5dc6
seanmckeever16 <seanmckeever@Seans-MacBook-Pro-2.local>
2015-05-23 01:45:24 +1200

    Cleaning up folders and adding jetpack to my scene

---
+++ Assets/Sean/Scripts/NewBurner.cs
@@ -0,0 +1,104 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBurner : MonoBehaviour {
+
+	public float flipNum = 80f;
+
+	public float rotationSpeed = 30f;
+	private GameObject player;
+	
+	public float maxSpeed = 32f;
+	public float minSpeed = 16f;
+
+	public float maxSize = 2f;
+	public float minSize = .7f;
+
+	private float tempSpeed;
+	private float tempSize;
+	
+	private PlayerMover playerMover;
+	private ParticleSystem partSys;
+
+	private float speedRange;
+	private float sizeRange;
+	// Use this for initialization
+	
+	private float h, v; //Input values
+	private Vector3 rotation;
+
+	private Quaternion toRotation;
+
+	private Vector3 targetPoint;
+
+	void Start () {
+		
+		player = GameObject.FindGameObjectWithTag ("Player");
+
+
+		partSys = gameObject.GetComponent<ParticleSystem> ();
+		playerMover = player.GetComponent<PlayerMover> ();
+		partSys.Stop ();
+
+		speedRange = maxSpeed - minSpeed;
+		sizeRange = maxSize - minSize;
+
+		rotation.Set (90f, 0f, 0f);
+		transform.localEulerAngles = rotation;
+
+		toRotation = Quaternion.LookRotation (new Vector3 (0f, -100f, 0f)); 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		h = Input.GetAxisRaw ("Horizontal");
+		v = Input.GetAxisRaw ("Vertical");
+
+		if (playerMover.jumpHold) {
+			tempSpeed = minSpeed + (speedRange * (playerMover.currentJump / playerMover.jumpRate));
+			partSys.startSpeed = tempSpeed;
+
+			tempSize = minSize + (sizeRange * (playerMover.currentJump / playerMover.jumpRate));
+			partSys.startSize = tempSize;
+		} else {
+			partSys.startSpeed = minSpeed;
+			partSys.startSize = minSize;
+		}
+
+		setRotation ();
+	
+		if (playerMover.jumping && (playerMover.jumpHold || (!(Mathf.Approximately (0f, h) && Mathf.Approximately (0f, v))))) {
+
+			transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
+			
+			if (partSys.isStopped)
+				partSys.Play ();
+
+		} else {
+			partSys.Stop ();
+			partSys.Clear();
+		}
+
+	}
+	
+
+	void setRotation() {
+
+		if (playerMover.jumpHold)
+			rotation.y = -100f;
+		else
+			rotation.y = -50f;
+
+
+		rotation.x = h * -100f;
+
+		rotation.z = v * -100f;
+
+		targetPoint = player.transform.TransformPoint (rotation);
+
+
+		toRotation = Quaternion.LookRotation (targetPoint - transform.position, transform.up);
+
+	}
+}
