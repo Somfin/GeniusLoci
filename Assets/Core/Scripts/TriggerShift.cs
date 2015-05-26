0d7291e1556d5134542b3a26258e780594aed56e
Adrian-Turjak <adriant@catalyst.net.nz>
2015-05-26 19:09:44 +1200

    new script for any direction shift

--- Assets/Core/Scripts/TriggerShift.cs
+++ Assets/Core/Scripts/TriggerShift.cs
@@ -1,29 +1,31 @@
 using UnityEngine;
 using System.Collections;
 
-public class TriggerHeightChange : TriggerBase
+public class TriggerShift : TriggerBase
 {
-	public float shiftDistance;
+	public float xShift;
+	public float yShift;
+	public float zShift;
 	public float speed;
 	public float dampening = 0.05f;
-
+	
 	private bool moving = false;
 	private Vector3 start;
 	private Vector3 target;
 	private float startTime;
 	private float journeyLength;
-
+	
 	void Start(){
 	}
-
+	
 	public override void Activate(){
 		startTime = Time.time;
 		start = transform.position;
-		target = new Vector3 (transform.position.x, transform.position.y + shiftDistance, transform.position.z);
+		target = new Vector3 (transform.position.x + xShift, transform.position.y + yShift, transform.position.z + zShift);
 		journeyLength = Vector3.Distance (transform.position, target);
 		moving = true;
 	}
-
+	
 	
 	void Update ()
 	{
