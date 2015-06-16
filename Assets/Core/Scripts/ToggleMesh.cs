e1185f0d0a2b6fc33847cea143445d9e7b2015c9
Adrian-Turjak <adriant@catalyst.net.nz>
2015-06-17 02:01:18 +1200

    Height raising thingy

---
+++ Assets/Core/Scripts/ToggleMesh.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ToggleMesh : TriggerBase
+{
+	public bool startHidden = true;
+	
+	void Start ()
+	{
+		if (startHidden) {
+			GetComponent<Renderer> ().enabled = false;
+		}
+	}
+	
+	// Can be triggered directly if needed by another trigger.
+	public override void Activate(){
+		GetComponent<Renderer> ().enabled = !GetComponent<Renderer> ().enabled;
+		TriggerChildren ();
+	}
+}
