b25513af821591fc2a25c9d33eecad27a9a88013
Adrian-Turjak <adriant@catalyst.net.nz>
2015-06-16 22:36:24 +1200

    adding logic to the first area

--- Assets/Core/Scripts/ToggleObject.cs
+++ Assets/Core/Scripts/ToggleObject.cs
@@ -5,6 +5,11 @@ public class ToggleObject : TriggerBase
 {
 	
 	public GameObject target;
+	public bool start_disabled = false;
+
+	void Start(){
+		target.SetActive (!start_disabled);
+	}
 	
 	public override void Activate(){
 		target.SetActive (!target.activeSelf);
