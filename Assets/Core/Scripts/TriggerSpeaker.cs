68afce45f14aa6b9a588e227f4ed18895831265d
Adrian-Turjak <adriant@catalyst.net.nz>
2015-06-19 20:50:04 +1200

    Adding one more shifting wall to area 1

---
+++ Assets/Core/Scripts/TriggerSpeaker.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (AudioSource))]
+public class TriggerSpeaker : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
