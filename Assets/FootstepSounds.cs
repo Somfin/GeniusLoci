d324edeec4c4b3b81cd2aae6dde8a3528695c124
Somfin <micr0softener@gmail.com>
2015-06-19 22:34:43 +1200

    Socket now makes noises!

---
+++ Assets/FootstepSounds.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FootstepSounds : MonoBehaviour {
+	public AudioClip[] footsteps;
+	public AudioSource source;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void Footstep() {
+		int step = Mathf.FloorToInt(Random.value * footsteps.Length);
+		source.clip = ((AudioClip)footsteps[step]);
+		source.Stop ();
+		source.Play ();
+	}
+}
