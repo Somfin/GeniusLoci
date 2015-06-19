9429ad80802f307eb1852a6a1e23f9d67d1de1fd
seanmckeever16 <seanmckeever@Seans-MacBook-Pro-2.local>
2015-06-19 14:33:20 +1200

    Landing light

---
+++ Assets/LaserSpotlight.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaserSpotlight : MonoBehaviour {
+
+	private GameObject player;
+	private PlayerMover playerMover;
+	private Light spotlight;
+	
+	// Use this for initialization
+	void Start () {
+
+		spotlight = GetComponent<Light> ();
+		spotlight.enabled = false;
+
+		player = GameObject.FindGameObjectWithTag ("Player");
+		playerMover = player.GetComponent<PlayerMover> ();
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (playerMover.jumping && (playerMover.currentJump < playerMover.jumpRate))
+			spotlight.enabled = true;
+		else
+			spotlight.enabled = false;
+
+	}
+}
