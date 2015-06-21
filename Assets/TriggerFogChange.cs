6951bf5782098c8d0bdac4bffd3cbdf503291e55
Somfin <micr0softener@gmail.com>
2015-06-21 16:46:29 +1200

    Some new systems in place. First scene starting to feel right.

---
+++ Assets/TriggerFogChange.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerFogChange : TriggerBase {
+	public float targetDensity;
+	public float changeRate;
+	public float currentDensity;
+	public Color targetColor;
+	public bool fogActive;
+	public bool changing;
+
+	// Use this for initialization
+	public override void Activate(){
+		changing = true;
+	}
+
+	void Update(){
+		currentDensity = RenderSettings.fogDensity;
+		if (changing) {
+			if (!RenderSettings.fog){
+				RenderSettings.fogDensity = 0.000000001f;
+			}
+			RenderSettings.fog = fogActive;
+			if (Mathf.Abs (RenderSettings.fogDensity - targetDensity) > 0.00001){
+				RenderSettings.fogDensity = Mathf.Lerp (RenderSettings.fogDensity, targetDensity, changeRate);
+				RenderSettings.fogColor = Color.Lerp (RenderSettings.fogColor, targetColor, changeRate);
+			} else {
+				RenderSettings.fogDensity = targetDensity;
+				changing = false;
+				TriggerChildren ();
+			}
+		}
+	}
+}
---
+++ Assets/TriggerLightChange.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerLightChange: TriggerBase {
+	public Light targetLight;
+	public float newIntensity;
+	public float shiftRate;
+	public bool changing;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	public override void Activate(){
+		changing = true;
+	}
+
+	void Update(){
+		if (changing) {
+			if (Mathf.Abs (targetLight.intensity - newIntensity) > 0.0001){
+				targetLight.intensity = Mathf.Lerp (targetLight.intensity, newIntensity, shiftRate);
+			} else {
+				targetLight.intensity = newIntensity;
+				changing = false;
+				TriggerChildren ();
+			}
+		}
+	}
+}
