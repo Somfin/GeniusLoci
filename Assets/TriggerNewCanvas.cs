f0566132f0f8c7655faece559bc1b656061d2cc3
Somfin <micr0softener@gmail.com>
2015-06-22 20:38:52 +1200

    Canvases complete, triggering works.

---
+++ Assets/TriggerNewCanvas.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerNewCanvas : TriggerBase {
+	public GameObject newCanvas;
+	
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public override void Activate() {
+		newCanvas.SetActive (true);
+		gameObject.SetActive (false);
+	}
+}
